2dda19f020e248312ab27d8dadb864dc9da5e1f9
AbdoSherif515 <145264588+AbdoSherif515@users.noreply.github.com>
2023-09-17 15:37:33 +0300

    Add files via upload

---
+++ Tasks/Debug/src/main.d
@@ -0,0 +1,13 @@
+src/main.o: ../src/main.c ../include/common_macros.h \
+ ../include/std_types.h ../include/RCC_interface.h \
+ ../include/GPIO_interface.h ../include/std_types.h
+
+../include/common_macros.h:
+
+../include/std_types.h:
+
+../include/RCC_interface.h:
+
+../include/GPIO_interface.h:
+
+../include/std_types.h:
