8910aa1adc81dbc7a5eac1f2324624f69df5efe8
AbdoSherif515 <145264588+AbdoSherif515@users.noreply.github.com>
2023-09-17 16:37:02 +0300

    Add files via upload

---
+++ Tasks/Task1/Debug/src/GPIO_program.d
@@ -0,0 +1,18 @@
+src/GPIO_program.o: ../src/GPIO_program.c ../include/std_types.h \
+ ../include/common_macros.h ../include/GPIO_config.h \
+ ../include/RCC_priv.h ../include/std_types.h ../include/GPIO_interface.h \
+ ../include/GPIO_priv.h
+
+../include/std_types.h:
+
+../include/common_macros.h:
+
+../include/GPIO_config.h:
+
+../include/RCC_priv.h:
+
+../include/std_types.h:
+
+../include/GPIO_interface.h:
+
+../include/GPIO_priv.h:
---
+++ Tasks/Task1/Debug/src/RCC_program.d
@@ -0,0 +1,17 @@
+src/RCC_program.o: ../src/RCC_program.c ../include/std_types.h \
+ ../include/common_macros.h ../include/RCC_config.h ../include/RCC_priv.h \
+ ../include/std_types.h ../include/RCC_priv.h ../include/RCC_interface.h
+
+../include/std_types.h:
+
+../include/common_macros.h:
+
+../include/RCC_config.h:
+
+../include/RCC_priv.h:
+
+../include/std_types.h:
+
+../include/RCC_priv.h:
+
+../include/RCC_interface.h:
